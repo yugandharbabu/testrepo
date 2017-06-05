eeb850f1020232e4b14ab71c92f9946716156872
Yugandhar Babu P <p.yugandharbabu@gmail.com>
2017-06-05 17:17:16 +0530

    Comment added

--- TestForms/Program.cs
+++ TestForms/Program.cs
@@ -17,6 +17,7 @@ namespace TestForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //added line
         }
     }
 }
